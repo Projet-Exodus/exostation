26e202a21f3ff5781c12d50c54dc7e542281263a
Thravalgur <thravalgur@gmail.com>
2025-04-27 10:04:55 +0200

    forme

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/looc.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/looc.tsx
@@ -4,7 +4,6 @@ import { CheckboxInput, FeatureToggle } from '../base';
 export const looc_admin_pref: FeatureToggle = {
   name: 'Voir LOOC',
   category: 'ADMIN',
-  description:
-    'Voir tous les messages LOOC.',
+  description: 'Voir tous les messages LOOC.',
   component: CheckboxInput,
 };
